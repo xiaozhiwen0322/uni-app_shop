44d1cb404d7f636bc866afe476cc64c9b60557a1
xiaozhiwen0322 <1809823474@qq.com>
2022-11-29 16:48:38 +0800

    完成首页功能

--- main.js
+++ main.js
@@ -1,21 +1,50 @@
 import App from './App'
 
+// 导入网络请求包
+import {
+	$http
+} from "@escook/request-miniprogram"
+uni.$http = $http
+$http.baseUrl = "https://api-ugo-web.itheima.net"
+
+// 请求拦截器
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: "数据加载中..."
+	})
+}
+// 响应拦截器
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
+//自定义请求失败提示框
+uni.$showMsg = function(title = "请求失败", duration = 1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none'
+
+	})
+}
+
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,19 +1,159 @@
 <template>
 	<view>
-		Home
+		<!-- 轮播图区域 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item v-for="item in bannerList" :key="item.goods_id">
+				<navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id">
+					<image :src="item.image_src" mode=""></image>
+				</navigator>
+			</swiper-item>
+		</swiper>
+
+		<!-- navItem区域 -->
+		<view class="nav-box">
+			<view v-for="(item,i) in navList" :key="i" class="nav-item" @click="navClickHandle(item.name)">
+				<image :src="item.image_src" mode="" class="nav-img"></image>
+			</view>
+		</view>
+		<!-- 楼层区域 -->
+		<view class="floor-list">
+			<view class="floor-item" v-for="(item,i) in floorList" :key="i">
+				<!-- 标题区 -->
+				<image :src="item.floor_title.image_src" class="title-image"></image>
+				<!-- 图片展示区 -->
+				<view class="image-show">
+					<!-- 左侧长图片区域 -->
+					<navigator :url="item.product_list[0].url">
+						<image :src="item.product_list[0].image_src"
+							:style="{width:item.product_list[0].image_width + 'rpx'}" mode="widthFix"></image>
+					</navigator>
+					<!-- 右侧四个小图片 -->
+					<view class="right-smallimg">
+						<navigator class="smallimg-item" v-for="(item2,i2) in item.product_list" :key="i2"
+							:url="item2.url" v-if="i2 !== 0">
+							<image :src="item2.image_src" :style="{width:item2.image_width + 'rpx'}" mode="widthFix">
+							</image>
+						</navigator>
+					</view>
+				</view>
+
+			</view>
+		</view>
 	</view>
+
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				
+				// 轮播图数据
+				bannerList: [],
+				// 分类导航数据
+				navList: [],
+				// 楼层数据
+				floorList: []
 			};
+		},
+		onLoad() {
+			this.getBannerList();
+			this.getnavList();
+			this.getFloorList()
+		},
+		methods: {
+			// 获取轮播图数据请求
+			async getBannerList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				if (res.meta.status !== 200) {
+					return uni.$showMsg()
+				} else {
+					this.bannerList = res.message
+				}
+			},
+			// 获取分类导航数据请求
+			async getnavList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/catitems');
+				if (res.meta.status !== 200) return uni.$showMsg();
+				this.navList = res.message
+			},
+			// 获取楼层数据请求
+			async getFloorList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/floordata');
+				if (res.meta.status !== 200) return uni.$showMsg();
+
+				// 处理数据
+				console.log(res);
+				res.message.forEach(floor => {
+					floor.product_list.forEach(prod => {
+						prod.url = prod.navigator_url = "/subpkg/goods_detail/goods_detail?" + prod
+							.navigator_url.split('?')[1]
+					})
+				})
+				this.floorList = res.message
+			},
+			// 分类导航点击事件
+			navClickHandle(name) {
+				if (name == "分类")
+					uni.switchTab({
+						url: "/pages/cate/cate"
+					})
+			}
 		}
+
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 330rpx;
 
+		.swiper-item,
+		image {
+			height: 100%;
+			width: 100%;
+		}
+	}
+
+	.nav-box {
+		display: flex;
+		justify-content: space-around;
+
+		.nav-item {
+			width: 128rpx;
+			margin: 15px;
+
+			.nav-img {
+				width: 100%;
+				height: 140rpx;
+			}
+		}
+	}
+
+	.floor-list {
+		.floor-item {
+			.title-image {
+				height: 36px;
+				width: 100%;
+			}
+		}
+
+		.image-show {
+			display: flex;
+			justify-content: space-between;
+			padding-left: 10rpx;
+
+			.right-smallimg {
+				width: 500rpx;
+				display: flex;
+				justify-content: space-around;
+				flex-wrap: wrap;
+			}
+		}
+	}
 </style>
