9f39006955886c51df4397682b0400c8b9de933d
xiaozhiwen0322 <1809823474@qq.com>
2022-12-08 18:13:53 +0800

    完成商品详情页

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,40 @@
 <template>
 	<view>
-		详情
+		<!-- 轮播图区域 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item v-for="(item,i) in goodsinfo.pics" :key="i" @click="preview(i)">
+				<image :src="item.pics_big"></image>
+			</swiper-item>
+		</swiper>
+		<!-- 主要信息 -->
+		<view class="primary">
+			<!-- 商品价格 -->
+			<view class="goods-price">
+				￥{{goodsinfo.goods_price}}
+			</view>
+			<!-- 商品名称及收藏 -->
+			<view class="name-star">
+				<!-- 商品名称 -->
+				<view class="goods-name">
+					{{goodsinfo.goods_name}}
+				</view>
+				<!-- 收藏 -->
+				<view class="collent">
+					<uni-icons type="star" size="25"></uni-icons>
+					<text>收藏</text>
+				</view>
+
+
+			</view>
+			<!-- 快递-免运费 -->
+			<view class="express">快递:免运费</view>
+
+		</view>
+		<rich-text :nodes="goodsinfo.goods_introduce"></rich-text>
+		<view class="goods-nav">
+			<uni-goods-nav :fill="true" :options="options" :buttonGroup="buttonGroup" @click="onClick"
+				@buttonClick="buttonClick" />
+		</view>
 	</view>
 </template>
 
@@ -8,12 +42,125 @@
 	export default {
 		data() {
 			return {
-
-			};
+				goodsinfo: {},
+				options: [{
+					icon: 'headphones',
+					text: '客服'
+				}, {
+					icon: 'shop',
+					text: '店铺',
+					info: 2,
+					infoBackgroundColor: '#007aff',
+					infoColor: "red"
+				}, {
+					icon: 'cart',
+					text: '购物车',
+					info: 2
+				}],
+				buttonGroup: [{
+						text: '加入购物车',
+						backgroundColor: '#ff0000',
+						color: '#fff'
+					},
+					{
+						text: '立即购买',
+						backgroundColor: '#ffa200',
+						color: '#fff'
+					}
+				]
+			}
+		},
+		methods: {
+			async getGoodsInfo(goods_id) {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/detail', {
+					goods_id
+				})
+				if (res.meta.status !== 200) return uni.$showMsg()
+				res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g,
+					'<img style="display:block"').replace(/webp /g, 'jpg')
+				this.goodsinfo = res.message
+			},
+			preview(i) {
+				uni.previewImage({
+					current: i,
+					urls: this.goodsinfo.pics.map(pic => {
+						return pic.pics_big
+					})
+				})
+			},
+			onClick(e) {
+				if (e.content.text == "购物车") {
+					uni.switchTab({
+						url: ('/pages/cart/cart')
+					})
+				}
+			}
+		},
+		onLoad(options) {
+			const goods_id = options.goods_id
+			this.getGoodsInfo(goods_id)
 		}
+
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 750rpx;
 
+		.swiper-item,
+		image {
+			width: 100%;
+			height: 100%;
+		}
+	}
+
+	.primary {
+		.goods-price {
+			font-size: 20px;
+			color: #9e0d16;
+			margin-top: 10px;
+			margin-left: 5px;
+		}
+
+		.name-star {
+			margin-top: 7px;
+			display: flex;
+			justify-content: space-between;
+
+			.goods-name {
+				width: 80%;
+				margin-left: 5px;
+				font-size: 15px;
+				align-items: center;
+				padding-right: 5px;
+				border-right: 1px solid #a2a2a2;
+			}
+
+			.collent {
+				display: flex;
+				flex-direction: column;
+				margin-right: 15px;
+				color: #a2a2a2;
+				font-size: 14px;
+				justify-content: space-around;
+				align-items: center;
+			}
+		}
+
+		.express {
+			color: #a2a2a2;
+			font-size: 14px;
+			margin-top: 7px;
+			margin-left: 5px;
+		}
+	}
+
+	.goods-nav {
+		position: sticky;
+		bottom: 0;
+		z-index: 999;
+	}
 </style>
